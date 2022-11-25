f994be3024a7512fc66b0a82989c52c5a55e6ac1
VRShard <flyerengine@gmail.com>
2022-11-25 12:18:19 +0800

    update: modify `material` colorscheme config according to the new mappings

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -7,41 +7,90 @@ require('material').setup({
         line_numbers = true, -- Enable contrast background for line numbers
         sign_column = true, -- Enable contrast background the sign column
         cursor_line = false, -- Enable darker background for the cursor line
-        non_current_windows = false, -- Enable darker background for non-current windows
+        non_current_windows = true, -- Enable darker background for non-current windows
         --popup_menu = true, -- Enable lighter background for the popup menu
     },
 
-    italics = {
-        comments = true, -- Enable italic comments
-        keywords = true, -- Enable italic keywords
-        functions = false, -- Enable italic functions
-        strings = false, -- Enable italic strings
-        variables = false -- Enable italic variables
+    styles = {
+        comments = { bold = false, italic = true },
+        strings = { bold = true, italic = true },
+        keywords = { bold = true, italic = false },
+        functions = { bold = false, italic = true },
+        variables = { bold = false, italic = false },
+        operators = { bold = true, italic = false },
+        types = { bold = true, italic = false },
+    },
+
+    plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        "dap",
+        --[[ "dashboard", ]]
+        "gitsigns",
+        --[[ "hop", ]]
+        --[[ "indent-blankline", ]]
+        "lspsaga",
+        --[[ "mini", ]]
+        --[[ "neogit", ]]
+        "nvim-cmp",
+        --[[ "nvim-navic", ]]
+        "nvim-tree",
+        --[[ "sneak", ]]
+        "telescope",
+        "trouble",
+        "which-key",
     },
 
     contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
         "terminal", -- Darker terminal background
         "packer", -- Darker packer background
-        "qf" -- Darker qf list background
+        "qf", -- Darker qf list background
+        "mason",
     },
 
     high_visibility = {
         lighter = true, -- Enable higher contrast text for lighter style
-        darker = false -- Enable higher contrast text for darker style
+        darker = true -- Enable higher contrast text for darker style
     },
 
     disable = {
+        colored_cursor = true,
         borders = false, -- Disable borders between verticaly split windows
         background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
         term_colors = false, -- Prevent the theme from setting terminal colors
-        eob_lines = false -- Hide the end-of-buffer lines
+        eob_lines = true -- Hide the end-of-buffer lines
     },
 
-    custom_highlights = {} -- Overwrite highlights with your own
+    async_loading = true,
+
+    lualine_style = "default",
+
+    custom_highlights = {
+        ["@something"] = {
+            bold = true,
+            italic = false,
+        },
+        ["Operator"] = {
+            bold = true,
+            italic = false,
+        },
+        ["MatchParen"] = {
+            bg = "#FFCB6B",
+            bold = true,
+        },
+        ["NotifyINFOTitle"] = {
+            fg = "#b2d582",
+        },
+        ["NotifyINFOIcon"] = {
+            fg = "#b2d582",
+        },
+    } -- Overwrite highlights with your own
 })
+--[[ vim.g.everforest_disable_terminal_colors = 1 ]]
+--[[ vim.g.everforest_background = 'hard' ]]
 local color_theme = "material"
-local status_ok, colorscheme = pcall(vim.cmd, "colorscheme " .. color_theme)
+--[[ vim.cmd "colorscheme material" ]]
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. color_theme)
 if not status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    vim.notify("colorscheme " .. color_theme .. " not found!")
     return
 end
