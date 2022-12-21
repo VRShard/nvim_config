9ae114f020c69bb5a0461b8dbbd90f10242a2779
VRShard <flyerengine@gmail.com>
2022-12-21 15:16:07 +0800

    fix(plugin): autopair plugin
     - Disable autopair plugin using treesitter parser, it seems not very
    accurate when need to tab out sometimes
     - Disable completion auto insert parentheses, it feels too overkill

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -5,7 +5,7 @@ if not status_ok then
 end
 
 npairs.setup {
-    check_ts = true,
+    check_ts = false,
     ts_config = {
         --lua = { "string", "source" },
         --javascript = { "string", "template_string" },
@@ -25,9 +25,9 @@ npairs.setup {
     },
 }
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-    return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+--[[ local cmp_autopairs = require "nvim-autopairs.completion.cmp" ]]
+--[[ local cmp_status_ok, cmp = pcall(require, "cmp") ]]
+--[[ if not cmp_status_ok then ]]
+--[[     return ]]
+--[[ end ]]
+--[[ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } }) ]]
