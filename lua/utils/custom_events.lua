1481aec63fdf0be58230406d869f7533c0a14592
VRShard <vierafia@gmail.com>
2023-12-24 06:13:29 +0800

    some additional lazy file events, in progress

--- lua/utils/custom_events.lua
+++ lua/utils/custom_events.lua
@@ -78,4 +78,67 @@ function M.lazy_file()
   })
 end
 
+function M.lazy_file_persistent()
+    -- Add support for the LazyFile event
+    local Event = require("lazy.core.handler.event")
+
+    -- We'll handle delayed execution of events ourselves
+    Event.mappings.LazyFilePersistent = { id = "LazyFilePersistent", event = "User", pattern = "LazyFilePersistent" }
+    Event.mappings["User LazyFilePersistent"] = Event.mappings.LazyFilePersistent
+
+
+  local events = {} ---@type {event: string, buf: number, data?: any}[]
+
+
+  local done = false
+  local function load()
+    if #events == 0 or done then
+      return
+    end
+    done = true
+    -- vim.api.nvim_del_augroup_by_name("lazy_file_persistent")
+
+
+    ---@type table<string,string[]>
+    local skips = {}
+    for _, event in ipairs(events) do
+      skips[event.event] = skips[event.event] or Event.get_augroups(event.event)
+    end
+
+
+    vim.api.nvim_exec_autocmds("User", { pattern = "LazyFilePersistent", modeline = false })
+    for _, event in ipairs(events) do
+      if vim.api.nvim_buf_is_valid(event.buf) then
+        Event.trigger({
+          event = event.event,
+          exclude = skips[event.event],
+          data = event.data,
+          buf = event.buf,
+        })
+        if vim.bo[event.buf].filetype then
+          Event.trigger({
+            event = "FileType",
+            buf = event.buf,
+          })
+        end
+      end
+    end
+    vim.api.nvim_exec_autocmds("CursorMoved", { modeline = false })
+    events = {}
+  end
+
+
+  -- schedule wrap so that nested autocmds are executed
+  -- and the UI can continue rendering without blocking
+  load = vim.schedule_wrap(load)
+
+
+  vim.api.nvim_create_autocmd(M.lazy_file_events, {
+    group = vim.api.nvim_create_augroup("lazy_file_persistent", { clear = true }),
+    callback = function(event)
+      table.insert(events, event)
+      load()
+    end,
+  })
+end
 return M
