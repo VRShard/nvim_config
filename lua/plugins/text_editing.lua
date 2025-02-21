987dcd4c51ef80c21c08c61b423f3f69fa2e8ffa
VRShard <vierafia@gmail.com>
2025-02-21 15:12:32 +0800

    feat(text-editing): add markdown rendering
     - use MeanderingProgrammer markdown-render plugin

--- lua/plugins/text_editing.lua
+++ lua/plugins/text_editing.lua
@@ -107,4 +107,15 @@ return {
             require("user.configs.cmp")
         end
     },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        event = "LazyFile",
+        ft = "markdown",
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
 }
